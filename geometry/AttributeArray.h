4a239a81927c1ea839dd90e55e88de4b18cb0e09
Mark Kellogg <mkkellogg@gmail.com>
2018-05-22 12:03:19 -0700

    Consolidating template classes into single files

--- geometry/AttributeArray.h
+++ geometry/AttributeArray.h
@@ -1,7 +1,11 @@
 #pragma once
 
+#include <new>
+#include <string.h>
+
 #include "../common/types.h"
 #include "../common/gl.h"
+#include "../common/assert.h"
 
 namespace Core {
 
@@ -12,32 +16,95 @@ namespace Core {
     const UInt32 componentCount = T::ComponentCount;
     GLuint bufferID;
 
-    void allocate();
-    void deallocate();
-    void buildGPUBuffer();
-    void destroyGPUBuffer();
-    void updateBufferData();
-    UInt32 getSize() const;
+    void allocate() {
+      this->deallocate();
+
+      this->storage = new (std::nothrow) typename T::ComponentType[this->attributeCount * T::ComponentCount];
+      ASSERT(this->storage != nullptr, "AttributeArray::allocate() -> Unable to allocate storage!");
+
+      Byte * tempAttributes =  new(std::nothrow) Byte[this->attributeCount * sizeof(T)];
+      ASSERT(tempAttributes != nullptr, "AttributeArray::allocate() -> Unable to allocate attributes!");
+
+      this->attributes = reinterpret_cast<T*>(tempAttributes);
+      for (UInt32 i = 0; i < this->attributeCount; i ++) {
+        new(this->attributes + i) T(this->storage + i);
+      }
+    }
+
+    void deallocate() {
+      if (this->attributes != nullptr) {
+        for (UInt32 i = 0; i < this->attributeCount; i++) {
+          this->attributes[i].~T();
+        }
+        delete this->attributes;
+        this->attributes = nullptr;
+      }
+      if (this->storage != nullptr) {
+        delete this->storage;
+        this->storage = nullptr;
+      }
+    }
+
+    void buildGPUBuffer() {
+      glGenBuffers(1, &this->bufferID);
+      this->updateBufferData();
+    }
+
+    void destroyGPUBuffer() {
+      glDeleteBuffers(1, &this->bufferID);
+    }
+
+    UInt32 getSize() const {
+      return this->attributeCount * T::ComponentCount * sizeof(typename T::ComponentType);
+    }
+
+    void updateBufferData() {
+      glBindBuffer(GL_ARRAY_BUFFER, this->bufferID);
+      glBufferData(GL_ARRAY_BUFFER, this->getSize(), this->storage, GL_DYNAMIC_DRAW);
+      glBindBuffer(GL_ARRAY_BUFFER, 0);
+    }
 
   public:
 
-    AttributeArray(UInt32 attributeCount);
-    virtual ~AttributeArray();
+    AttributeArray(UInt32 attributeCount): storage(nullptr), attributes(nullptr), attributeCount(attributeCount) {
+      allocate();
+      buildGPUBuffer();
+    }
 
-    UInt32 getAttributeCount() const;
-    UInt32 getComponentCount() const;
-    typename T::ComponentType * getStorage();
-    T* getAttributes();
-    T& getAttribute(UInt32 index);
-    void store(const typename T::ComponentType * data);
-    GLuint getGLBufferID() const;
+    ~AttributeArray() {
+      destroyGPUBuffer();
+      deallocate();
+    }
 
-    // forward declaration
-    class iterator;
+    GLuint getGLBufferID() const {
+      return this->bufferID;
+    }
 
-    iterator begin();
-    iterator end();
+    UInt32 getComponentCount() const {
+      return this->componentCount;
+    }
 
+    UInt32 getAttributeCount() const {
+      return this->attributeCount;
+    }
+
+    typename T::ComponentType * getStorage() {
+      return this->storage;
+    }
+
+    T* getAttributes() {
+      return this->attributes;
+    }
+
+    T& getAttribute(UInt32 index) {
+      return this-attributes[index];
+    }
+
+    void store(const typename T::ComponentType * data) {
+      memcpy(this->storage, data, this->getSize());
+      this->updateBufferData();
+    }
+    
     class iterator {
       AttributeArray<T> * array;
       UInt32 index;
@@ -73,7 +140,13 @@ namespace Core {
 
     };
 
+    iterator begin() {
+      return iterator(this, 0);
+    };
+
+    iterator end() {
+      return iterator(this, this->getAttributeCount());
+    };
+
   };
 }
-
-#include "AttributeArray.cpp"
--- geometry/Vector2Components.h
+++ geometry/Vector2Components.h
@@ -10,14 +10,21 @@ namespace Core {
     T& x;
     T& y;
 
-    Vector2Components(T* data, const T& x, const T& y);
-    virtual ~Vector2Components() = 0;
+    Vector2Components(T* data, const T& x, const T& y): x(data[0]), y(data[1]) {
+      this->set(x, y);
+    }
 
-    void set(const T& x, const T& y);
-    void copy(const Vector2Components<T>& src);
+    ~Vector2Components() {}
+
+    void set(const T& x, const T& y) {
+      this->x = x;
+      this->y = y;
+    }
+
+    void copy(const Vector2Components<T>& src) {
+      this->set(src.x, src.y);
+    }
 
   };
 
 }
-
-#include "Vector2Components.cpp"
