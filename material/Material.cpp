b48ca3c3c4d5ccce63e7ad3618be59a60b426448
Mark Kellogg <mkkellogg@gmail.com>
2018-06-16 00:51:47 -0700

    Changover from std::weak_ptr to WeakPointer in Material complete

--- material/Material.cpp
+++ material/Material.cpp
@@ -1,31 +1,30 @@
 #include <memory>
 
 #include "Material.h"
-#include "../util/WeakPointer.h"
 #include "../common/debug.h"
 
 namespace Core {
 
-    Material::Material(std::weak_ptr<Graphics> graphics) : graphics(graphics), ready(false) {
+    Material::Material(WeakPointer<Graphics> graphics) : graphics(graphics), ready(false) {
     }
 
-    Material::Material(std::weak_ptr<Graphics> graphics, std::weak_ptr<Shader> shader): Material(graphics) {
+    Material::Material(WeakPointer<Graphics> graphics, WeakPointer<Shader> shader): Material(graphics) {
         this->setShader(shader);
         WeakPointer<Shader> shaderPtr(shader);
         this->ready = shaderPtr && shaderPtr->isReady();
     }
 
-    void Material::setShader(std::weak_ptr<Shader> shader) {
+    void Material::setShader(WeakPointer<Shader> shader) {
         this->shader = shader;
     }
 
-    std::weak_ptr<Shader> Material::getShader() {
+    WeakPointer<Shader> Material::getShader() {
         return this->shader;
     }
 
     Bool Material::buildFromSource(const std::string& vertexSource, const std::string& fragmentSource) {
          WeakPointer<Graphics> graphicsPtr(this->graphics);
-        std::weak_ptr<Shader> shader = graphicsPtr->createShader(vertexSource, fragmentSource);
+        WeakPointer<Shader> shader = graphicsPtr->createShader(vertexSource, fragmentSource);
         WeakPointer<Shader> shaderPtr(shader);
         Bool success = shaderPtr->build();
         if (!success) {
--- material/Material.h
+++ material/Material.h
@@ -1,6 +1,8 @@
 #pragma once
 
 #include <memory>
+
+#include "../util/WeakPointer.h"
 #include "../Graphics.h"
 #include "Shader.h"
 #include "StandardAttributes.h"
@@ -9,9 +11,9 @@
 namespace Core {
   class Material {
   public:
-    Material(std::weak_ptr<Graphics> graphics);
-    Material(std::weak_ptr<Graphics> graphics, std::weak_ptr<Shader> shader);
-    std::weak_ptr<Shader> getShader();
+    Material(WeakPointer<Graphics> graphics);
+    Material(WeakPointer<Graphics> graphics, WeakPointer<Shader> shader);
+    WeakPointer<Shader> getShader();
     virtual Bool build() = 0;
 
     virtual Int32 getShaderLocation(StandardAttributes attribute) = 0;
@@ -19,10 +21,10 @@ namespace Core {
     virtual void sendCustomUniformsToShader() = 0;
 
   protected:
-    std::weak_ptr<Graphics> graphics;
-    std::weak_ptr<Shader> shader;
+    WeakPointer<Graphics> graphics;
+    WeakPointer<Shader> shader;
     Bool ready;
     Bool buildFromSource(const std::string& vertexSource, const std::string& fragmentSource);
-    void setShader(std::weak_ptr<Shader> shader);
+    void setShader(WeakPointer<Shader> shader);
   };
 }
