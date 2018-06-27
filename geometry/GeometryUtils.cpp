61afb7ef16010e6be9d293f3a19fb2afe008336a
Mark Kellogg <mkkellogg@gmail.com>
2018-06-27 15:54:42 -0700

    Adedd GeometryUtils

---
+++ geometry/GeometryUtils.cpp
@@ -0,0 +1,11 @@
+#include "GeometryUtils.h"
+
+namespace Core {
+
+    WeakPointer<Mesh> GeometryUtils::createGrid(Real width, Real height, UInt32 hSubDivisions, UInt32 vSubDivisions) {
+        WeakPointer<Mesh> gridMesh;
+
+        return gridMesh;
+    }
+
+}
---
+++ geometry/GeometryUtils.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include "../common/types.h"
+#include "../util/WeakPointer.h"
+
+namespace Core {
+
+    // forward declarations
+    class Mesh;
+
+    class GeometryUtils {
+    public:
+       static  WeakPointer<Mesh> createGrid(Real width, Real height, UInt32 hSubDivisions, UInt32 vSubDivisions);
+     
+    private:
+        
+    };
+}
