dcba64d40c506985e4400563e97157c1710097aa
Mark Kellogg <mkkellogg@gmail.com>
2018-06-28 13:52:54 -0700

    Fixing GeometryUtils::createGrid() (again)

--- geometry/GeometryUtils.cpp
+++ geometry/GeometryUtils.cpp
@@ -29,8 +29,13 @@ namespace Core {
 
         for (UInt32 cx = 0; cx <= hCells; cx++) {
             for (UInt32 cy = 0; cy <= vCells; cy++) {
-                Real bx = (Real)cx * cellWidth + left;
-                Real by = (Real)cy * cellHeight + bottom;
+                Real xOffset = (Real)cx * cellWidth;
+                Real yOffset = (Real)cy * cellHeight;
+                Real bx = xOffset + left;
+                Real by = yOffset + bottom;
+
+                Real uvX = xOffset / hWorldToTex;
+                Real uvY = yOffset / vWorldToTex;
 
                 positions.push_back(bx);
                 positions.push_back(by);
@@ -42,9 +47,6 @@ namespace Core {
                 normals.push_back(1);
                 normals.push_back(0);
 
-                Real uvX = bx / hWorldToTex;
-                Real uvY = by / vWorldToTex;
-
                 uvs.push_back(uvX);
                 uvs.push_back(uvY);
             }
@@ -55,18 +57,19 @@ namespace Core {
                 Real bx = (Real)cx * cellWidth + left;
                 Real by = (Real)cy * cellHeight + bottom;
 
-                UInt32 i0 = cx * vCells + cy;
+                UInt32 i0 = cx * (vCells + 1) + cy;
                 UInt32 i1 = i0 + 1;
-                UInt32 i2 = (cx + 1) * vCells + cy;
+                UInt32 i2 = (cx + 1) * (vCells + 1) + cy;
                 UInt32 i3 = i2 + 1;
 
                 indices.push_back(i0);
-                indices.push_back(i3);
                 indices.push_back(i1);
+                indices.push_back(i3);
 
                 indices.push_back(i0);
-                indices.push_back(i2);
                 indices.push_back(i3);
+                indices.push_back(i2);
+
             }
         }
 
@@ -75,12 +78,11 @@ namespace Core {
         WeakPointer<Engine> engine = Engine::instance();
 
         WeakPointer<Mesh> gridPlane = engine->createMesh(vertexCount, hCells * vCells * 6);
-        gridPlane->init();
 
         gridPlane->enableAttribute(Core::StandardAttribute::Position);
         Core::Bool positionInited = gridPlane->initVertexPositions();
         gridPlane->getVertexPositions()->store(positions.data());
-
+        
         gridPlane->enableAttribute(Core::StandardAttribute::Normal);
         Core::Bool normalsInited = gridPlane->initVertexNormals();
         gridPlane->getVertexNormals()->store(normals.data());
