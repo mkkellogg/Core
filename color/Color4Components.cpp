ec67bd4621781890fe60369dd1b7af3befbdd5c5
Mark Kellogg <mkkellogg@gmail.com>
2020-12-28 09:39:00 -0800

    Real params do not need to be references

--- color/Color4Components.cpp
+++ color/Color4Components.cpp
@@ -2,14 +2,14 @@
 
 namespace Core {
 
-    Color4Components::Color4Components(Real* data, const Real& r, const Real& g, const Real& b, const Real& a) : r(data[0]), g(data[1]), b(data[2]), a(data[3]) {
+    Color4Components::Color4Components(Real* data, Real r, Real g, Real b, Real a) : r(data[0]), g(data[1]), b(data[2]), a(data[3]) {
         this->set(r, g, b, a);
     }
 
     Color4Components::~Color4Components() {
     }
 
-    void Color4Components::set(const Real& r, const Real& g, const Real& b, const Real& a) {
+    void Color4Components::set(Real r, Real g, Real b, Real a) {
         this->r = r;
         this->g = g;
         this->b = b;
--- color/Color4Components.h
+++ color/Color4Components.h
@@ -11,9 +11,9 @@ namespace Core {
         Real& b;
         Real& a;
 
-        Color4Components(Real* data, const Real& r, const Real& g, const Real& b, const Real& a);
+        Color4Components(Real* data, Real r, Real g, Real b, Real a);
         virtual ~Color4Components() = 0;
 
-        void set(const Real& r, const Real& g, const Real& b, const Real& a);
+        void set(Real r, Real g, Real b, Real a);
     };
 }
